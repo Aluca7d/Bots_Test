b4ff7e843f36d5d7dce9cccdd002edacd819ce03
March Coronado <57552569+Aluca7d@users.noreply.github.com>
2023-07-25 21:00:07 -0400

    Bots_Discord_Test

--- MarckxBot.py
+++ MarckxBot.py
@@ -1,19 +1,50 @@
-import pandas as pd 
-import unicorn_binance_websocket_api as uni 
+# MarrckxBot.py
+from lib2to3.pgen2 import token
+from operator import truediv
+import os
+import random
+from aiohttp import ClientError
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
 
-bwsam = uni.BinanceWebSocketApiManager(exchange= "binance.com")
+intents = discord.Intents.default()
+intents.members =True
 
-bwsam.create_stream("kline_1m", "BTCUSDT", output="UnicornFy")
-
-def obtener_precio(data):
     
-    time= pd.to_datetime(data["event_time"], unit="ms")
-    precio = float(data["kline"]["close_price"])
-    
-    print(F"Price Bitcoin: {precio}")
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT')
+GUILD = os.getenv('DISCORD_GUILD')
 
-while True:
-    data = bwsam.pop_stream_data_from_stream_buffer()
+MarckxBot = commands.Bot(command_prefix='>',intents=intents) 
 
-    if data and len(data) > 3:
-     obtener_precio(data)
+@MarckxBot.event
+async def on_ready():
+
+    Servidor = discord.utils.find(lambda Server: Server.name == GUILD, MarckxBot.guilds)
+
+    print(
+        f'\n {MarckxBot.user} Acabo de depertar Sr.Coronado.\n'
+        f'\n Estoy disponible en el: {Servidor.name} ( id: {Servidor.id})'
+    )
+    \
+    '''
+    Usuarios_Server = '\n - '.join([Usuarios_Server.name for Usuarios_Server in Servidor.members])
+    print(f'\n Usuarios en el servidor: \n - {Usuarios_Server}')
+
+    '''  
+@MarckxBot.command(name = "Hola Marck")
+async def prueba(ctx):
+    response = "Hola Sr.Coronado, acabo de nacer"
+    await ctx.send(response)
+
+@MarckxBot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise   
+
+MarckxBot.run(TOKEN)
+
